788945fa9209915ace6b96a1ea277e3050d92481
Matvey Pavlov <75501963+Pavlomat@users.noreply.github.com>
2022-07-17 14:35:25 +0300

    Update MainPresenter.swift

--- MVP Project/MVP Project/MainModule/Presenter/MainPresenter.swift
+++ MVP Project/MVP Project/MainModule/Presenter/MainPresenter.swift
@@ -15,31 +15,31 @@ protocol MainViewProtocol: AnyObject {
 protocol MainViewPresenterProtocol: AnyObject {
     init(view: MainViewProtocol, networkService: NetworkServiceProtocol, router: RouterProtocol)
     func getComments()
-    var comments: [Comment]? { get set } //чобы можно было не только получать но и менять полученные данные
+    var comments: [Comment]? { get set }
     func tapOnTheComment(comment: Comment?)
 }
 
 class MainPresenter: MainViewPresenterProtocol {
-    weak var view: MainViewProtocol?    // weak чтобы исбежать утечек памяти (всегда)
-    var router: RouterProtocol? //если сделать weak то приложение упадет
+    weak var view: MainViewProtocol?
+    var router: RouterProtocol?
     let networkService: NetworkServiceProtocol!
     var comments: [Comment]?
     
-    required init(view: MainViewProtocol, networkService: NetworkServiceProtocol, router: RouterProtocol) { //это инициализатор, что происходит при включении программы (вроде как)
+    required init(view: MainViewProtocol, networkService: NetworkServiceProtocol, router: RouterProtocol) {
         self.view = view
         self.networkService = networkService
         self.router = router
         getComments()
     }
     
-    func tapOnTheComment(comment: Comment?) { //по тапу на строку срабатывает этот метод в презентере, презентер вызывает роутер, роутер вызывает этот метод, получаем коммент
+    func tapOnTheComment(comment: Comment?) {
         router?.showDetail(comment: comment)
     }
     
     func getComments() {
-        networkService.getComments { [weak self] result in //weak чобы исбежать утечек памяти
+        networkService.getComments { [weak self] result in
             guard let self = self else { return }
-            DispatchQueue.main.async { //это все на передний план а то будет краш приложения, тк обработка UIв сегда на переднем плане
+            DispatchQueue.main.async {
                 switch result {
                 case .success(let comments):
                     self.comments = comments
